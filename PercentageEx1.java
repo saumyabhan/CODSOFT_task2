ee0c5901e21d18efb1ff28527d699441bcb41dbb
saumyabhan <133961694+saumyabhan@users.noreply.github.com>
2023-09-14 22:18:13 +0530

    Add files via upload

---
+++ PercentageEx1.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.Scanner;
+public class PercentageEx1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        int totalMarks = 0;
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + ": ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+
+        System.out.println("Total Marks: " + totalMarks);
+
+        double averagePercentage = (double) totalMarks / numSubjects;
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+
+        String grade = calculateGrade(averagePercentage);
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+
+    public static String calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return "A+";
+        } else if (averagePercentage >= 80) {
+            return "A";
+        } else if (averagePercentage >= 70) {
+            return "B";
+        } else if (averagePercentage >= 60) {
+            return "C";
+        } else if (averagePercentage >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
+
+
